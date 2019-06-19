2f96addf52bdaf8281ed34345ec705a5f908dbea
Devin Leonhart <devinleonhart@fastmail.com>
2019-06-19 16:54:00 -0700

    Updated social links.

--- template-parts/content-social-buttons.php
+++ template-parts/content-social-buttons.php
@@ -11,34 +11,34 @@
 <div class="buttons">
   <ul>
     <li class="picarto">
-      <a href=""></a>
+      <a href="https://picarto.tv/Nekonny"></a>
     </li>
     <li class="fur-affinity">
-      <a href=""></a>
+      <a href="http://www.furaffinity.net/user/freelancemanga"></a>
     </li>
     <li class="deviant-art">
-      <a href=""></a>
+      <a href="https://freelancemanga.deviantart.com/"></a>
     </li>
     <li class="instagram">
       <a href=""></a>
     </li>
     <li class="youtube">
-      <a href=""></a>
+      <a href="https://www.youtube.com/user/TheDrawingNeko"></a>
     </li>
     <li class="patreon">
-      <a href=""></a>
+      <a href="https://www.patreon.com/nekonny"></a>
     </li>
     <li class="paypal">
-      <a href=""></a>
+      <a href="https://paypal.me/nekonny"></a>
     </li>
     <li class="ko-fi">
-      <a href=""></a>
+      <a href="https://ko-fi.com/nekonny"></a>
     </li>
     <li class="gumroad">
-      <a href=""></a>
+      <a href="https://gumroad.com/nekonny"></a>
     </li>
     <li class="twitter">
-      <a href=""></a>
+      <a href="https://twitter.com/Nekonny"></a>
     </li>
     <li class="discord">
       <a href=""></a>
