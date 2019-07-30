a681714d60264b968e7e73495661a1ff059aadf0
Devin Leonhart <devinleonhart@fastmail.com>
2019-07-30 15:49:55 -0700

    Update nekonny cross promotion links

--- template-parts/content-cross-promotion.php
+++ template-parts/content-cross-promotion.php
@@ -13,8 +13,8 @@
   $url = home_url( add_query_arg( array(), $wp->request ) );
   if ( strpos($url, '###') !== false || strpos($url, 'paprika') !== false || strpos($url, 'imew') !== false) : ?>
   <section class="cross-promotion">
-    <a class="spotlight" href="https://nekonny.katbox.net/comics/imew"></a>
-    <a class="spotlight" href="https://pmp.nekonny.com"></a>
+    <a class="spotlight" href="https://imew.kemono.cafe"></a>
+    <a class="spotlight" href="https://pmp.kemono.cafe"></a>
     <a class="spotlight" href="https://nekonny.com/store.html"></a>
   </section>
 <?php elseif ( strpos($url, 'kemono-cafe-comic.local') !== false || strpos($url, 'rascals') !== false ) : ?>
