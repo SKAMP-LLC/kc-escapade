b076f5b005abcc1450be0d9c6ed5c4bddf8bc37a
Devin Leonhart <devinleonhart@fastmail.com>
2019-08-28 12:00:01 -0700

    Add Knighthood banner

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,9 +1,11 @@
 import Siema from 'siema';
 
 document.addEventListener("DOMContentLoaded", () => {
+  const number_of_comics = 8;
   const comicSlots = new Siema({
     perPage: 7,
-    loop: true
+    loop: true,
+    startIndex: Math.floor(Math.random() * number_of_comics),
   });
   document.querySelector('.previous_slot').addEventListener('click', () => comicSlots.prev());
   document.querySelector('.next_slot').addEventListener('click', () => comicSlots.next());
--- template-parts/content-carousel.php
+++ template-parts/content-carousel.php
@@ -32,26 +32,8 @@
     <div class="comic_slot slot_6">
       <a href="https://laslindas.kemono.cafe/"></a>
     </div>
-    <div class="comic_slot slot_0">
-      <a href="https://imew.kemono.cafe/"></a>
-    </div>
-    <div class="comic_slot slot_1">
-      <a href="https://paprika.kemono.cafe/"></a>
-    </div>
-    <div class="comic_slot slot_2">
-      <a href="https://cb.kemono.cafe/"></a>
-    </div>
-    <div class="comic_slot slot_3">
-      <a href="https://pmp.kemono.cafe/"></a>
-    </div>
-    <div class="comic_slot slot_4">
-      <a href="https://rascals.kemono.cafe/"></a>
-    </div>
-    <div class="comic_slot slot_5">
-      <a href="https://theeye.kemono.cafe/"></a>
-    </div>
-    <div class="comic_slot slot_6">
-      <a href="https://laslindas.kemono.cafe/"></a>
+    <div class="comic_slot slot_7">
+      <a href="https://knighthood.kemono.cafe/"></a>
     </div>
     <div class="comic_slot slot_0">
       <a href="https://imew.kemono.cafe/"></a>
@@ -74,6 +56,9 @@
     <div class="comic_slot slot_6">
       <a href="https://laslindas.kemono.cafe/"></a>
     </div>
+    <div class="comic_slot slot_7">
+      <a href="https://knighthood.kemono.cafe/"></a>
+    </div>
     <div class="comic_slot slot_0">
       <a href="https://imew.kemono.cafe/"></a>
     </div>
@@ -94,6 +79,33 @@
     </div>
     <div class="comic_slot slot_6">
       <a href="https://laslindas.kemono.cafe/"></a>
+    </div>
+    <div class="comic_slot slot_7">
+      <a href="https://knighthood.kemono.cafe/"></a>
+    </div>
+    <div class="comic_slot slot_0">
+      <a href="https://imew.kemono.cafe/"></a>
+    </div>
+    <div class="comic_slot slot_1">
+      <a href="https://paprika.kemono.cafe/"></a>
+    </div>
+    <div class="comic_slot slot_2">
+      <a href="https://cb.kemono.cafe/"></a>
+    </div>
+    <div class="comic_slot slot_3">
+      <a href="https://pmp.kemono.cafe/"></a>
+    </div>
+    <div class="comic_slot slot_4">
+      <a href="https://rascals.kemono.cafe/"></a>
+    </div>
+    <div class="comic_slot slot_5">
+      <a href="https://theeye.kemono.cafe/"></a>
+    </div>
+    <div class="comic_slot slot_6">
+      <a href="https://laslindas.kemono.cafe/"></a>
+    </div>
+    <div class="comic_slot slot_7">
+      <a href="https://knighthood.kemono.cafe/"></a>
     </div>
   </div>
   <div class="control next_slot"></div>
