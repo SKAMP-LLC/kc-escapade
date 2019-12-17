6d32573259d0a04dfa1dd1d69ffaad5f7047cbe8
Devin Leonhart <devinleonhart@fastmail.com>
2019-12-17 14:14:21 -0800

    Remove wukrii baners.

--- template-parts/content-ad-banner-ver.php
+++ template-parts/content-ad-banner-ver.php
@@ -9,5 +9,8 @@
 ?>
 
 <div class="ad-side-banner">
-  <script type="text/javascript" src="https://www.comicad.net/r/i97cj7JFDd/"></script>
+  <script type="text/javascript" src="https://www.comicad.net/r/6Bcj0095JF/"></script>
+</div>
+<div class="kc-side-banner">
+  <a href="https://kemono.cafe/">
 </div>
