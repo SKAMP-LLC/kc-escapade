4fc9f9c02a4414b60d29bb113b16a58908d73443
Devin Leonhart <devinleonhart@fastmail.com>
2020-02-24 15:30:28 -0800

    Burndown list.

--- header.php
+++ header.php
@@ -130,6 +130,7 @@
     <?php endif; ?>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="p:domain_verify" content="a98b66571218a24e706bc7e200e08b55"/>
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
