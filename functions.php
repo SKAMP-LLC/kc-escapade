faeac01090ea2b3ead15f7c83cf9ced70d73e8ce
Devin Leonhart <devinleonhart@fastmail.com>
2020-05-06 13:44:46 -0700

    Fix rules for non-page width comics.

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@
  */
 
    // Choices are: paprika | imew | rascals | theeye | cb | pmp | laslindas | knighthood | addictivescience | tots
-   $development_theme = '###';
+   $development_theme = 'laslindas';
 
 /**
  * Escapade only works in WordPress 4.4 or later.
