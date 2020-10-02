022583a0f3f3feec31338f7066ea1f900df3b4dc
Devin Leonhart <devinleonhart@fastmail.com>
2020-10-02 15:30:04 -0700

    Add NSFW modal + cookie support.

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,22 @@
-function destroyModal () {
-  document.querySelector('#comic').classList.remove('nsfw');
-  document.querySelector('.modal').remove();
-}
+import Cookies from 'js-cookie';
 
-document.addEventListener('DOMContentLoaded', () => {
+function createModal () {
   if (document.body.contains(document.querySelector('.modal'))) {
     document.querySelector('#comic').classList.add('nsfw');
     document.querySelector('.modal').addEventListener('click', () => destroyModal());
+  }
+}
+
+function destroyModal () {
+  document.querySelector('#comic').classList.remove('nsfw');
+  document.querySelector('.modal').remove();
+  Cookies.set('show-nsfw', true, { expires: 365 });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (Cookies.get('show-nsfw') === 'true') {
+    destroyModal();
+  } else {
+    createModal();
   }
 });
