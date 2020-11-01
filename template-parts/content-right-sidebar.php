37663cc1e0fcad9d08901d37c864f53a4ad8c28a
Devin Leonhart <devinleonhart@fastmail.com>
2020-10-31 19:28:55 -0700

    twc and image udpate

--- template-parts/content-right-sidebar.php
+++ template-parts/content-right-sidebar.php
@@ -61,6 +61,9 @@
           <a href="https://kemono.cafe/"></a>
         </div>
       <ul class="partners">
+        <li class="twc">
+          <a href="https://www.topwebcomics.com/vote/26468"></a>
+        </li>
         <li class="twokinds">
           <a href="http://twokinds.keenspot.com"></a>
         </li>
@@ -111,6 +114,9 @@
           <a href="https://kemono.cafe/"></a>
         </div>
       <ul class="partners">
+        <li class="twc">
+          <a href="https://www.topwebcomics.com/vote/26469"></a>
+        </li>
       </ul>
     </div>
 
@@ -155,10 +161,13 @@
         <li class="artificialincident">
           <a href="https://artificialincident.com"></a>
         </li>
+        <li class="twc">
+          <a href="https://www.topwebcomics.com/vote/8890"></a>
+        </li>
       </ul>
     </div>
 
-  <?php elseif ( $development_theme == 'rascals' || $development_theme == 'knuckleup' || strpos($url, 'rascals') || strpos($url, 'knuckle-up') !== false ) : ?>
+  <?php elseif ( $development_theme == 'rascals' || strpos($url, 'rascals') ) : ?>
     <div>
       <ul class="buttons">
         <li class="picarto">
@@ -190,6 +199,9 @@
           <a href="https://kemono.cafe/"></a>
         </div>
       <ul class="partners">
+        <li class="twc">
+          <a href="https://www.topwebcomics.com/vote/9929"></a>
+        </li>
         <li class="twokinds">
           <a href="http://twokinds.keenspot.com"></a>
         </li>
@@ -214,7 +226,113 @@
       </ul>
     </div>
 
-    <?php elseif ( $development_theme == 'theeye' || $development_theme == 'tots' || strpos($url, 'theeye') !== false || strpos($url, 'tots') !== false ) : ?>
+    <?php elseif ( $development_theme == 'knuckleup' || strpos($url, 'knuckle-up') !== false ) : ?>
+    <div>
+      <ul class="buttons">
+        <li class="picarto">
+          <a href="https://picarto.tv/mastergodai"></a>
+        </li>
+        <li class="fur-affinity">
+          <a href="http://www.furaffinity.net/user/mastergodai/"></a>
+        </li>
+        <li class="deviant-art">
+          <a href="https://www.deviantart.com/mastergodai"></a>
+        </li>
+        <li class="instagram">
+          <a href="https://www.instagram.com/mastergodai/"></a>
+        </li>
+        <li class="youtube">
+          <a href="https://www.youtube.com/user/playorgodai"></a>
+        </li>
+        <li class="patreon">
+          <a href="https://www.patreon.com/Mastergodai"></a>
+        </li>
+        <li class="gumroad">
+          <a href="https://gumroad.com/mastergodai"></a>
+        </li>
+        <li class="twitter">
+          <a href="https://twitter.com/Mastergodai"></a>
+        </li>
+      </ul>
+      <div class="kc-side-banner">
+          <a href="https://kemono.cafe/"></a>
+        </div>
+      <ul class="partners">
+        <li class="twc">
+          <a href="https://www.topwebcomics.com/vote/26468"></a>
+        </li>
+        <li class="twokinds">
+          <a href="http://twokinds.keenspot.com"></a>
+        </li>
+        <li class="thedepthscomic">
+          <a href="http://thedepthscomic.com"></a>
+        </li>
+        <li class="yoshsaga">
+          <a href="https://yoshsaga.com"></a>
+        </li>
+        <li class="artificialincident">
+          <a href="https://www.artificialincident.com"></a>
+        </li>
+        <li class="draconiachronicles">
+          <a href="https://www.draconiachronicles.com/"></a>
+        </li>
+        <li class="wukrii">
+          <a href="https://www.wukrii.com/"></a>
+        </li>
+        <li class="iotters">
+          <a href="https://tapas.io/series/IOtters"></a>
+        </li>      
+      </ul>
+    </div>
+
+    <?php elseif ( $development_theme == 'theeye' || strpos($url, 'theeye') !== false ) : ?>
+      <div>
+        <ul class="buttons">
+          <li class="picarto">
+            <a href="https://picarto.tv/avencri"></a>
+          </li>
+          <li class="fur-affinity">
+            <a href="https://www.furaffinity.net/user/avencri/"></a>
+          </li>
+          <li class="deviant-art">
+            <a href="https://www.deviantart.com/avencri"></a>
+          </li>
+          <li class="patreon">
+            <a href="https://www.patreon.com/avencri"></a>
+          </li>
+          <li class="paypal">
+            <a href="https://www.paypal.com/paypalme2/avencri"></a>
+          </li>
+          <li class="ko-fi">
+            <a href="https://ko-fi.com/Avencri"></a>
+          </li>
+          <li class="twitter">
+            <a href="https://twitter.com/avencri"></a>
+          </li>
+        </ul>
+        <div class="kc-side-banner">
+          <a href="https://kemono.cafe/"></a>
+        </div>
+        <ul class="partners">
+          <li class="twc">
+            <a href="https://www.topwebcomics.com/vote/26468"></a>
+          </li>
+          <li class="mylifewithfel">
+            <a href="https://mylifewithfel.com"></a>
+          </li>
+          <li class="wukrii">
+            <a href="https://www.wukrii.com/"></a>
+          </li>
+          <li class="mousechievous">
+            <a href="https://studiokhimera.com/mousechievous/"></a>
+          </li>
+          <li class="uberquest">
+            <a href="https://studiokhimera.com/uberquest/"></a>
+          </li>
+        </ul>
+      </div>
+
+      <?php elseif ( $development_theme == 'tots' || strpos($url, 'tots') !== false ) : ?>
       <div>
         <ul class="buttons">
           <li class="picarto">
@@ -293,10 +411,63 @@
           <a href="https://kemono.cafe/"></a>
         </div>
         <ul class="partners">
+          <li class="twc">
+            <a href="https://www.topwebcomics.com/vote/26468"></a>
+          </li>
         </ul>
       </div>
 
-      <?php elseif ( $development_theme == 'laslindas' || $development_theme == 'knighthood' || strpos($url, 'laslindas') !== false || strpos($url, 'knighthood') !== false ) : ?>
+      <?php elseif ( $development_theme == 'laslindas' || strpos($url, 'laslindas') !== false ) : ?>
+      <div>
+        <ul class="buttons">
+          <li class="picarto">
+            <a href="https://picarto.tv/Chalodraws"></a>
+          </li>
+          <li class="fur-affinity">
+            <a href="http://www.furaffinity.net/user/chalodillo/"></a>
+          </li>
+          <li class="deviant-art">
+            <a href="https://www.deviantart.com/chalosan"></a>
+          </li>
+          <li class="patreon">
+            <a href="https://www.patreon.com/Chalodillo"></a>
+          </li>
+          <li class="ko-fi">
+            <a href="https://ko-fi.com/chalodillo"></a>
+          </li>
+          <li class="twitter">
+            <a href="https://twitter.com/Chalodillo"></a>
+          </li>
+        </ul>
+        <div class="kc-side-banner">
+          <a href="https://kemono.cafe/"></a>
+        </div>
+        <ul class="partners">
+          <li class="twc">
+            <a href="https://www.topwebcomics.com/vote/13696"></a>
+          </li>
+          <li class="twokinds">
+            <a href="http://twokinds.keenspot.com"></a>
+          </li>
+          <li class="mylifewithfel">
+            <a href="https://mylifewithfel.com"></a>
+          </li>
+          <li class="themonsterunderthebed">
+            <a href="https://themonsterunderthebed.net"></a>
+          </li>
+          <li class="bittersweetcandybowl">
+            <a href="https://bittersweetcandybowl.com"></a>
+          </li>
+          <li class="draconiachronicles">
+            <a href="https://www.draconiachronicles.com/"></a>
+          </li>
+          <li class="wukrii">
+            <a href="https://www.wukrii.com/"></a>
+          </li>
+        </ul>
+      </div>
+
+      <?php elseif ( $development_theme == 'knighthood' || strpos($url, 'knighthood') !== false ) : ?>
       <div>
         <ul class="buttons">
           <li class="picarto">
