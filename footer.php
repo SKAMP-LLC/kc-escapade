c3b466ff6a53be6c546865a547b3a90aaaa460ac
Devin Leonhart <devinleonhart@fastmail.com>
2022-05-02 16:33:29 -0700

    Update year in footer.

--- footer.php
+++ footer.php
@@ -17,59 +17,59 @@
     global $development_theme;
     if ( $development_theme == 'paprika' || strpos($url, 'paprika') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>Paprika &#169; 2021 Nekonny All Rights Reserved</span>
+        <span>Paprika &#169; 2022 Nekonny All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'imew' || strpos($url, 'imew') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>iMew &#169; 2021 Nekonny All Rights Reserved</span>
+        <span>iMew &#169; 2022 Nekonny All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'cb' || strpos($url, 'cb') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>Caribbean Blue &#169; 2021 Nekonny All Rights Reserved</span>
+        <span>Caribbean Blue &#169; 2022 Nekonny All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'pmp' || strpos($url, 'pmp') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>Practice Makes Perfect &#169; 2021 Nekonny All Rights Reserved</span>
+        <span>Practice Makes Perfect &#169; 2022 Nekonny All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'rascals' || strpos($url, 'rascals') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>Rascals &#169; 2021 Mastergodai All Rights Reserved</span>
+        <span>Rascals &#169; 2022 Mastergodai All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'knuckleup' || strpos($url, 'knuckle-up') !== false ) : ?>
     <footer id="colophon" class="site-footer" role="contentinfo">
-      <span>Knuckle Up &#169; 2021 Mastergodai All Rights Reserved</span>
+      <span>Knuckle Up &#169; 2022 Mastergodai All Rights Reserved</span>
     </footer>
   <?php elseif ( $development_theme == 'theeye' || strpos($url, 'theeye') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>The Eye of Ramalach &#169; 2021 Avencri All Rights Reserved</span>
+        <span>The Eye of Ramalach &#169; 2022 Avencri All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'knighthood' || strpos($url, 'knighthood') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>Knighthood &#169; 2021 Chalo All Rights Reserved</span>
+        <span>Knighthood &#169; 2022 Chalo All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'laslindas' || strpos($url, 'laslindas') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>Las Lindas &#169; 2021 Chalo All Rights Reserved</span>
+        <span>Las Lindas &#169; 2022 Chalo All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'addictivescience' || strpos($url, 'addictivescience') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>Addictive Science &#169; 2021 Cervelet All Rights Reserved</span>
+        <span>Addictive Science &#169; 2022 Cervelet All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'tots' || strpos($url, 'tots') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>Tina of the South &#169; 2021 Avencri All Rights Reserved</span>
+        <span>Tina of the South &#169; 2022 Avencri All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'bethellium' || strpos($url, 'bethellium') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>Bethellium &#169; 2021 ABlueDeer All Rights Reserved</span>
+        <span>Bethellium &#169; 2022 ABlueDeer All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'princessbunny' || strpos($url, 'princessbunny') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>Princess Bunny &#169; 2021 bun-bun.art All Rights Reserved</span>
+        <span>Princess Bunny &#169; 2022 bun-bun.art All Rights Reserved</span>
       </footer>
   <?php elseif ( $development_theme == 'ultrarosa-esp' || $development_theme == 'ultrarosa' || strpos($url, 'ultrarosa-esp') !== false || strpos($url, 'ultrarosa') !== false ) : ?>
       <footer id="colophon" class="site-footer" role="contentinfo">
-        <span>Ultra Rosa &#169; Sr.Kah All Rights Reserved</span>
+        <span>Ultra Rosa &#169; 2022 Sr.Kah All Rights Reserved</span>
       </footer>
   <?php endif; ?>
 </div><!-- .site -->
